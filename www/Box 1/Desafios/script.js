4ca6db83e28f3d56abab1f734d1bbf1ec871feea
Marcelo Silva <82605444+marcelossilvaa@users.noreply.github.com>
2023-02-17 10:09:26 -0300

    Adicionando botao avançar

--- www/Box 1/Desafios/script.js
+++ www/Box 1/Desafios/script.js
@@ -1,8 +1,15 @@
 
 function resolverEnigma(){
-
+    var resposta = ''
     const str = document.getElementById('tentativa').value
+    const botao = document.getElementById("botao-surgir")
+
+    const newBtn = document.createElement('button')
+    newBtn.innerText = ''
+    newBtn.id = 'btnGo'
+
     var resposta = document.getElementById('resposta')
+
 
     com_acento = "áàãââÁÀÃÂéêÉÊíÍóõôÓÔÕúüÚÜçÇABCDEFGHIJKLMNOPQRSTUVXWYZ";
     sem_acento = "aaaaaaaaaeeeeiioooooouuuuccabcdefghijklmnopqrstuvxwyz";
@@ -21,9 +28,14 @@ function resolverEnigma(){
         }
     }
     
-    if(novastr == 'educacao financeira'){
+    if(novastr == 'teste'){
         resposta.innerHTML = 'Parabéns, você acertou!'
+        botao.appendChild(newBtn)
+        
     } else {
         resposta.innerHTML = 'Você errou, tente novamente.'
     }
 }
+
+//Novo botão
+
